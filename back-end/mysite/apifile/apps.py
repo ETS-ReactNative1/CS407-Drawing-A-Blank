d85f6d1cd98246b70fea8bfbf46e79c50d41ccbc
Jack Dawes <jack.dawes@warwick.ac.uk>
2021-11-03 13:28:30 +0000

    django skeleton set up
    The basic api skeleton

---
+++ back-end/mysite/apifile/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApifileConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apifile'
---
+++ back-end/mysite/apifile/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# 'python manage.py makemigrations' 'python manage.py migrate'
+# run in terminal after changing/making new model, then register in admin.py
+
+# example model:
+# class Hero(models.Model):
+
+#     name = models.CharField(max_length=60)
+#     alias = models.CharField(max_length=60)
+
+#     def __str__(self):
+#         return self.name
+
