21899ee370113b1cf08a4e2d25fc85efd617385f
Adam Platt <adamjonplatt@gmail.com>
2021-11-11 22:41:53 +0000

    Changed grid functions to work off numeric easting/northing by default rather than converting. Added db func to gridsVisible

--- back-end/mysite/apifile/grids.py
+++ back-end/mysite/apifile/grids.py
@@ -3,6 +3,7 @@ import numpy as np
 from bresenham import bresenham
 from pyproj import Transformer
 from geopy import distance
+from models import Grid
 
 """
 bng is main library used: https://pypi.org/project/bng/
@@ -22,6 +23,7 @@ Fixed by first converted to all numeric so that the library converts it to the c
 
 """
 
+
 def latLongDistance(pointA, pointB):
     """
     Function input: 2 tuples each containing latitude and longitude
@@ -35,13 +37,14 @@ def latLongDistance(pointA, pointB):
     """
     return distance.geodesic(pointA, pointB, ellipsoid='WGS-84').meters
 
+
 def calculateSpeed(pointA, pointB, timeInbetween):
     """
     Function input: 2 tuples each containing latitude and longitude and the time taken to get from point A to point B
     Function output: the calculated speed in meters per unit time(for example if timeInbetween was in seconds then output is X m/s).
     """
-    distance = latLongDistance(pointA,pointB)
-    return distance/timeInbetween
+    distance = latLongDistance(pointA, pointB)
+    return distance / timeInbetween
 
 
 def gridToLatlong(grid):
@@ -52,15 +55,12 @@ def gridToLatlong(grid):
     Function output: A tuple containing converted latitude and longitude (both floats).
     """
 
-
-    #defines the transformation from UK ordinance survey to lat long
+    # defines the transformation from UK ordinance survey to lat long
     transformer = Transformer.from_crs("EPSG:27700", "EPSG:4326")
 
-    #converts to numeric only grid references
+    # converts to numeric only grid references
     x, y = bng.to_osgb36(grid)
     return transformer.transform(x, y)
-
-
 
 
 def latlongToGrid(latlong):
@@ -69,46 +69,38 @@ def latlongToGrid(latlong):
     Function input: A tuple containing converted latitude and longitude (both floats): Requires a couple of decimal places for accuracy
     Function output: Grid reference of BNG as a string returns 2 letters + 10 digits.
     """
-    #defines the transformation from lat long to UK ordinance survey
-    transformer = Transformer.from_crs("EPSG:4326","EPSG:27700")
+    # defines the transformation from lat long to UK ordinance survey
+    transformer = Transformer.from_crs("EPSG:4326", "EPSG:27700")
 
-    #Converts to grid, specified 10 figs for accuracy.
+    # Converts to grid, specified 10 figs for accuracy.
     x, y = transformer.transform(latlong[0], latlong[1])
-    return bng.from_osgb36((x,y),figs=10)
+    return bng.from_osgb36((x, y), figs=10)
 
 
-
-
-def latlongsOfGrid(grid,distance=1):
-
+def bounds_of_grid(location, distance=1):
     """
     Returns the latitude and longitudes of the input grid. 
     The distance argument determines how large the grid is e.g. distance=1 means the grid is 1x1 meters.
 
-    Function input: Grid reference as String
+    Function input: Grid reference as easting and northing tuple
     Function output: A list of tuples containing converted latitude and longitude coordinates for all 4 corners of the current grid.
     """
 
-    #Extract eastings and northings and make numerical only for easy manipulation.
-    eastings, northings = bng.to_osgb36(grid)
-
+    easting, northing = location
 
     coordinates = []
 
-
-    #grid references refer to the bottom left corner of the grid so need to get positively adjacent grids coordinates.
-    grids = [(0,0),(distance,0),(distance,distance),(0,distance)]
+    # grid references refer to the bottom left corner of the grid so need to get positively adjacent grids coordinates.
+    grids = [(0, 0), (distance, 0), (distance, distance), (0, distance)]
     for i in range(len(grids)):
-        newEastings = eastings +grids[i][0]
-        newNorthings = northings + grids[i][1]
+        new_eastings = easting + grids[i][0]
+        new_northings = northing + grids[i][1]
 
-        #convert to 2 letter + 10 digit form to convert to latlong
-        newGrid = bng.from_osgb36((newEastings, newNorthings), figs=10)
-        coordinates.append(gridToLatlong(newGrid))
+        # convert to 2 letter + 10 digit form to convert to latlong
+        new_grid = bng.from_osgb36((new_eastings, new_northings), figs=10)
+        coordinates.append(gridToLatlong(new_grid))
 
     return coordinates
-
-
 
 
 def points_in_circle_np(radius, x0=0, y0=0):
@@ -117,7 +109,7 @@ def points_in_circle_np(radius, x0=0, y0=0):
     """
     x_ = np.arange(x0 - radius - 1, x0 + radius + 1, dtype=int)
     y_ = np.arange(y0 - radius - 1, y0 + radius + 1, dtype=int)
-    x, y = np.where((x_[:,np.newaxis] - x0)**2 + (y_ - y0)**2 <= radius**2)
+    x, y = np.where((x_[:, np.newaxis] - x0) ** 2 + (y_ - y0) ** 2 <= radius ** 2)
     # x, y = np.where((np.hypot((x_-x0)[:,np.newaxis], y_-y0)<= radius)) # alternative implementation
     for x, y in zip(x_[x], y_[y]):
         yield x, y
@@ -128,33 +120,31 @@ def gridsInRadius(position, radius=4):
     Function input: Current latitude longitude position
     Function output: A list of grids within the radius.
     """
-    x,y = bng.to_osgb36(position)
-    grids = points_in_circle_np(radius,x,y) #seems fine for now, could set the function's center at 0,0 and just add the offset to the origin of the square.
+    x, y = position
+    grids = points_in_circle_np(radius, x, y)  # seems fine for now, could set the function's center at 0,0 and just
+    # add the offset to the origin of the square.
     grids = list(map(lambda p: bng.from_osgb36(p, figs=10), grids))
     return grids
 
 
-
-def gridsInPath(positionA, positionB):
+def gridsInPath(point_a, point_b):
     """
-    Function input: Current and old latitude longitude position
+    Function input: Current and old easting and northing tuples
     Function output: A list of grids within the straight line path.
 
     Library: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
     """
-    xA,yA = bng.to_osgb36(positionA)
-    xB,yB = bng.to_osgb36(positionB)
 
-    #Using the bresenham line algorithm implemented using a library.
-    grids = list(bresenham(xA,yA,xB,yB))
+    east_a, north_a = point_a
+    east_b, north_b = point_b
 
-    #https://stackoverflow.com/questions/10212445/map-list-item-to-function-with-arguments
+    # Using the bresenham line algorithm implemented using a library.
+    grids = list(bresenham(east_a, north_a, east_b, north_b))
+
+    # https://stackoverflow.com/questions/10212445/map-list-item-to-function-with-arguments
     grids = list(map(lambda p: bng.from_osgb36(p, figs=10), grids))
 
-
     return grids
-
-
 
 
 def gridsVisible(coords):
@@ -162,25 +152,24 @@ def gridsVisible(coords):
     Function input: 4 longitude/latitude coordinates that the screen can see
     Function output: coordinates of every grid that is visible.
     """
-    #if quadrilateral and bottomleft < topRight then only need two coords.
+    # if quadrilateral and bottomleft < topRight then only need two coords.
     bottomLeft = coords[0]
     topRight = coords[2]
 
     blGrid = latlongToGrid(bottomLeft)
     trGrid = latlongToGrid(topRight)
 
-    eastBL, northBL = bng.to_osgb36(blGrid)
-    eastTR, northTR = bng.to_osgb36(trGrid)
+    lower_east, lower_north = bng.to_osgb36(blGrid)
+    upper_east, upper_north = bng.to_osgb36(trGrid)
 
     allCoords = []
 
-    #this is quite slow: zooming out means theres a lot of grids and repeated coordinates/calculations
-    #Could fix by "super sampling" grids e.g. 4 1x1m grids average their colour to make 1 4x4m grid. (only need to access colour) or by saving coordinates to not calculate again.
-    #Then you can access less coordinates and do less calculations.
-    for i in range(northBL,northTR+1):
-        for j in range(eastBL,eastTR+1):
-            currentGrid = bng.from_osgb36((j,i), figs=10)
-            colour = "red" #TODO: get colour from database?
-            allCoords.append({"colour": colour, "coords": latlongsOfGrid(currentGrid)})
+    # this is quite slow: zooming out means theres a lot of grids and repeated coordinates/calculations Could fix by
+    # "super sampling" grids e.g. 4 1x1m grids average their colour to make 1 4x4m grid. (only need to access colour)
+    # or by saving coordinates to not calculate again. Then you can access less coordinates and do less calculations.
+    tiles = Grid.objects.filter(northing__lte=upper_north, northing__gte=lower_north,
+                                easting__lte=upper_east, easting__gte=upper_east)
+    for tile in tiles:
+        allCoords.append({"colour": tiles.colour, "bounds": bounds_of_grid((tile.easting, tile.northing))})
 
     return allCoords
