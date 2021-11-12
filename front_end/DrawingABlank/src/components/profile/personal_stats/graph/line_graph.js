a6477c2a6bff1795d216159737d05b1070338af8
TBCNC <chasjohnh@gmail.com>
2021-11-12 18:40:26 +0000

    Added a basic bar graph

--- front_end/DrawingABlank/src/components/profile/personal_stats/graph/line_graph.js
+++ front_end/DrawingABlank/src/components/profile/personal_stats/graph/line_graph.js
@@ -11,34 +11,29 @@ class WorkoutLineGraph extends Component{
         return(
             <View>
                 <Text style={styles.title}>{this.props.graphTitle}</Text>
-                <View style={styles.graph}>
-
-                </View>
-                <View style={styles.sub_data}>
-                    <View style={{ height: 250, padding: 0, flexDirection: 'row' }}>
-                        <YAxis
+                <View style={{ height: this.props.height, padding: 0, flexDirection: 'row' }}>
+                    <YAxis
+                        data={this.props.yData}
+                        style={{ marginBottom: xAxisHeight }}
+                        contentInset={verticalContentInset}
+                        svg={axesSvg}
+                    />
+                    <View style={{ flex: 1, marginLeft: 10 }}>
+                        <LineChart
+                            style={{ flex: 1 }}
                             data={this.props.yData}
-                            style={{ marginBottom: xAxisHeight }}
                             contentInset={verticalContentInset}
+                            svg={{ stroke: 'rgb(134, 65, 244)' }}
+                        >
+                            <Grid/>
+                        </LineChart>
+                        <XAxis
+                            style={{ marginHorizontal: -10, height: xAxisHeight }}
+                            data={this.props.yData}
+                            formatLabel={this.props.xFunction}
+                            contentInset={{ left: 10, right: 10 }}
                             svg={axesSvg}
                         />
-                        <View style={{ flex: 1, marginLeft: 10 }}>
-                            <LineChart
-                                style={{ flex: 1 }}
-                                data={this.props.yData}
-                                contentInset={verticalContentInset}
-                                svg={{ stroke: 'rgb(134, 65, 244)' }}
-                            >
-                                <Grid/>
-                            </LineChart>
-                            <XAxis
-                                style={{ marginHorizontal: -10, height: xAxisHeight }}
-                                data={this.props.yData}
-                                formatLabel={(value, index) => index}
-                                contentInset={{ left: 10, right: 10 }}
-                                svg={axesSvg}
-                            />
-                        </View>
                     </View>
                 </View>
             </View>
