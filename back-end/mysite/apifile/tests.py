4fd93d4dcde1399e170c43078873b569cb33d538
Tal-Four <adamjonplatt@gmail.com>
2021-11-17 21:46:22 +0000

    Added a 2nd team to demonstrate retrieving different colours

--- back-end/mysite/apifile/tests.py
+++ back-end/mysite/apifile/tests.py
@@ -7,10 +7,12 @@ from . import models
 # Create your tests here.
 class GridTestCase(TestCase):
     def setUp(self):
-        models.Team.objects.create(name="Test Team", colour="FF0000")
-        team = models.Team.objects.get(name="Test Team")
+        models.Team.objects.create(name="Test Team1", colour="FF0000")
+        models.Team.objects.create(name="Test Team2", colour="00FF00")
+        team = models.Team.objects.get(name="Test Team1")
+        team2 = models.Team.objects.get(name="Test Team2")
         models.Grid.objects.create(easting="431890", northing="265592", team=team)
-        models.Grid.objects.create(easting="431890", northing="265593", team=team)
+        models.Grid.objects.create(easting="431890", northing="265593", team=team2)
         models.Grid.objects.create(easting="432315", northing="265866", team=team)
         models.Grid.objects.create(easting="431932", northing="265511", team=team)
         models.Grid.objects.create(easting="432360", northing="265781", team=team)
