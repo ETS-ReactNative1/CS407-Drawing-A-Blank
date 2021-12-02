b0d43afb03ba45dc0a6d7454ff5be95436697ca2
Tal-Four <adamjonplatt@gmail.com>
2021-12-02 13:43:22 +0000

    updated db connection

--- back-end/mysite/mysite/settings.py
+++ back-end/mysite/mysite/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'drawabdb',
+        'USER': 'drawab',
+        'PASSWORD': 'MX37mtd4m7nR',
+        'HOST': 'mysql.dcs.warwick.ac.uk',
     }
 }
 
