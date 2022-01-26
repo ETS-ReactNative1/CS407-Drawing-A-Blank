0b8144253140d61e817964f8a9d48487f4d68541
Tal-Four <adamjonplatt@gmail.com>
2022-01-26 16:52:02 +0000

    changed grid back to custom viewset query

--- back-end/mysite/apifile/views.py
+++ back-end/mysite/apifile/views.py
@@ -303,8 +303,8 @@ def calc_calories(workout_type, dur):
 
 class GridView(viewsets.ViewSet):
 
-    #@action(methods=['post'], detail=True)
-    def create(self, request):
+    @action(methods=['post'], detail=True)
+    def query(self, request):
         data = request.data
         bl = data['bottom_left']
         br = data['bottom_right']
