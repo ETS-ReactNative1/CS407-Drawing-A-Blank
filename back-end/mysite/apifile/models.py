4fc1e5a60262944629bcd441eed616c49959256d
Tal-Four <adamjonplatt@gmail.com>
2022-01-26 21:17:16 +0000

    fixing migrations pt3

--- back-end/mysite/apifile/models.py
+++ back-end/mysite/apifile/models.py
@@ -48,10 +48,10 @@ class Item(models.Model):
 # alter the base django user table with extra fields
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    age = models.PositiveIntegerField(null=True)
-    gender = models.CharField(max_length=16, blank=True)
-    height = models.FloatField(null=True)
-    weight = models.FloatField(null=True)
+    #age = models.PositiveIntegerField(null=True)
+    #gender = models.CharField(max_length=16, blank=True)
+    #height = models.FloatField(null=True)
+    #weight = models.FloatField(null=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     items = models.ManyToManyField(Item)
 
