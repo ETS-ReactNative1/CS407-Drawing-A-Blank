abb7ebaf88b3767775a3c2939a20acc4af9516b3
Minhyuk <u1804670@live.warwick.ac.uk>
2022-01-27 01:49:15 +0000

    all grids in event

--- back-end/mysite/apifile/events.py
+++ back-end/mysite/apifile/events.py
@@ -7,6 +7,28 @@ import mahotas
 import numpy as np
 
 #event clear
+def clear_event_grids(event):
+    grids = all_grids_in_event(event)
+
+
+def all_grids_in_event(event):
+
+    bounds = event.get_bounds()
+    xs, ys = zip(*bounds)
+    minx, maxx = min(xs), max(xs)
+    miny, maxy = min(ys), max(ys)
+
+    newPoly = [(int(x - minx), int(y - miny)) for (x, y) in bounds]
+
+    X = maxx - minx + 1
+    Y = maxy - miny + 1
+
+    #https://stackoverflow.com/questions/21339448/how-to-get-list-of-points-inside-a-polygon-in-python
+    grid = np.zeros((X, Y), dtype=np.int8)
+    mahotas.polygon.fill_polygon(newPoly, grid)
+
+    #https://www.desmos.com/calculator/mb6ktvx1az
+    return [(x + minx-1, y + miny) for (x, y) in zip(*np.nonzero(grid))]
 
 
 
