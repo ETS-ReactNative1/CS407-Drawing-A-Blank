df56b4144dff875386340120a95077aad0334ba8
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-02-03 18:47:47 +0000

    editing placeholders
    added comments to describe functions of the placeholders

--- back-end/mysite/apifile/cron.py
+++ back-end/mysite/apifile/cron.py
@@ -1,7 +1,13 @@
 # placeholder for when events end
 def event_end():
+    # get all events
+    # check if over (will already ended ones still be stored?)
+    # if over call ending methods for that event id
     pass
 
 # placeholder to empty token table of very expired tokens
 def purge_tokens():
+    # get all tokens in token table
+    # if token.created is older than x
+    # delete
     pass
