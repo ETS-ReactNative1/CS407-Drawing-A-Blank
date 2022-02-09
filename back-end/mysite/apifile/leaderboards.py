fcebee1bd9be960af81a97950c915e444afa32ad
Minhyuk <u1804670@live.warwick.ac.uk>
2022-02-09 21:18:12 +0000

    fixed var names for clarity

--- back-end/mysite/apifile/leaderboards.py
+++ back-end/mysite/apifile/leaderboards.py
@@ -10,9 +10,8 @@ def distance_leaderboard(time_range):
     
     #get all workout points greater than the input time.
     workout_points = WorkoutPoint.objects.filter(time__gt=time_range)
-    print(workout_points[0].workout.player_id)
 
-    #initialize the dictionary.
+    #initialize the dictionary/hashmap.
     players = Player.objects.all()
     distance_leaderboard = {}
     for player in players:
@@ -21,32 +20,34 @@ def distance_leaderboard(time_range):
 
 
     #group workout points by their workout id (probably a better way to do this)
-    workouts = []
+    workouts_list = []
     cur_workout = workout_points[0].workout
-    cur_workout_points = []
+    cur_points = []
     for point in workout_points:
         #new workout
         if(cur_workout!=point.workout):
-            workouts.append(cur_workout_points)
-            cur_workout_points=[]
+            workouts_list.append(cur_points)
+            cur_points=[]
             cur_workout =point.workout
 
-        cur_workout_points.append(point)
+        cur_points.append(point)
     #add the last workout
-    if(len(cur_workout_points)>0):
-        workouts.append(cur_workout_points)
+    if(len(cur_points)>0):
+        workouts_list.append(cur_points)
 
 
     #go through list of workouts and calculate distance per workout and add it to the respective player.
-    for workout in workouts:
+    for workout in workouts_list:
         dist = 0.0
+
+        #go through all points in a workout and get the distance
         cur_point = (workout[0].easting,workout[0].northing)
         for point in workout[1:]:
             dist += distance(cur_point,(point.easting,point.northing))
             cur_point = (point.easting,point.northing)
         distance_leaderboard[workout[0].workout.player] +=dist
 
-
+    #sort by distance. https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
     return {k: v for k, v in sorted(distance_leaderboard.items(), key=lambda item: item[1],reverse=True)}
 
 
