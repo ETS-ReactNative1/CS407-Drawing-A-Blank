fe34954e96a11c22657f93e013eae41353421bb7
Tal-Four <adamjonplatt@gmail.com>
2022-02-10 20:00:25 +0000

    added code to store winners and distribute coins to players

--- back-end/mysite/apifile/cron.py
+++ back-end/mysite/apifile/cron.py
@@ -1,5 +1,5 @@
 from datetime import date, timedelta
-from .models import Event
+from .models import Event, Team, EventStandings, EventPerformance
 
 
 def event_check():
@@ -16,8 +16,46 @@ def event_check():
     # check event ends
     to_end = Event.object.filter(start__gte=today, end__lt=tomorrow)
     for event in to_end:
-        # store info
+        # get winners
+        winners = event.winners()
+        unseen_teams = {"terra", "ocean", "windy"}
+        teams = {"terra": Team.objects.get(name="terra"),
+                 "ocean": Team.objects.get(name="ocean"),
+                 "windy": Team.objects.get(name="windy")}
+
+        if len(winners) == 3:
+            first = teams[winners[0].name]
+            second = teams[winners[1].name]
+            third = teams[winners[2].name]
+        elif len(winners) == 2:
+            first = teams[winners[0].name]
+            unseen_teams.remove(winners[0].name)
+            second = teams[winners[1].name]
+            unseen_teams.remove(winners[1].name)
+            third = teams[unseen_teams.pop()]
+        elif len(winners) == 1:
+            first = teams[winners[0].name]
+            unseen_teams.remove(winners[0].name)
+            second = teams[unseen_teams.pop()]
+            third = teams[unseen_teams.pop()]
+        else:
+            first = teams[unseen_teams.pop()]
+            second = teams[unseen_teams.pop()]
+            third = teams[unseen_teams.pop()]
+
+        EventStandings.objects.create(event=event, first=first,
+                                      second=second, third=third)
         # rewards
+        players = EventPerformance.objects.select_related('player__team').filter(event=event)
+        for playerPerf in players:
+            if playerPerf.player.team.name == first.name:
+                coins_gained = 3 * playerPerf.contribution
+            elif playerPerf.player.team.name == second.name:
+                coins_gained = 2 * playerPerf.contribution
+            else:
+                coins_gained = 1 * playerPerf.contribution
+            playerPerf.player.coins += coins_gained
+            playerPerf.player.save()
         # clear area
         event.clear_area()
 
