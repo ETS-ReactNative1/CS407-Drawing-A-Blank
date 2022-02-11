f800193a1623a3771012e6ddb1dbe372b03161b5
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-02-11 12:50:17 +0000

    returning username

--- back-end/mysite/apifile/models.py
+++ back-end/mysite/apifile/models.py
@@ -53,7 +53,7 @@ class Player(models.Model):
     def points(time):
         # 
         # .values('username','team', 'avatar')
-        return Player.objects.values('user', 'team').filter(workout__workoutpoint__time__gte=time).annotate(points=Count('workout__workoutpoint')).order_by('-points')
+        return Player.objects.values('user__username', 'team').filter(workout__workoutpoint__time__gte=time).annotate(points=Count('workout__workoutpoint')).order_by('-points')
 
 
 class Grid(models.Model):
