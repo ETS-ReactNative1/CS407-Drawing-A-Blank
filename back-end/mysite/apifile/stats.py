581a744bbb29063b0a75c658c124c0e41890f488
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-02-18 12:34:18 +0000

    testing

--- back-end/mysite/apifile/stats.py
+++ back-end/mysite/apifile/stats.py
@@ -30,9 +30,9 @@ def user_total_distance(input_name):
 
 def user_total_points(input_name):
 
-    workouts = Workout.objects.filter(player__user__username=input_name).annotate(score=Sum('workout__points'))
+    workouts = Workout.objects.filter(player__user__username=input_name).annotate(score=Sum('points'))
 
-    return workouts[0].score
+    return workouts[1].score
 
 
 def calc_workout_distance(input_workout):
