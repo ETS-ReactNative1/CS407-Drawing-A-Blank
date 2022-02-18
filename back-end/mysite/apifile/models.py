93991a47beba1c2423052621cc5d431d3628bcdc
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-02-18 13:09:34 +0000

    optimising

--- back-end/mysite/apifile/models.py
+++ back-end/mysite/apifile/models.py
@@ -55,15 +55,25 @@ class Player(models.Model):
     def points(time, teams):
         
         if(teams==None or teams ==[]):
-            players = Player.objects.values('user__username', 'team__name')
-            workouts = Workout.objects.filter(workoutpoint__time__gt=time).distinct()
-            # return Player.objects.values('user__username', 'team__name').filter(workout__workoutpoint__time__gte=time).distinct().annotate(points=Count('workout__points')).order_by('-points')
+            # players = Player.objects.values('user__username')
+            # workouts = Workout.objects.filter(workoutpoint__time__gt=time).distinct()
+            players = Player.objects.values('user__username', 'team__name').filter(workout__workoutpoint__time__gte=time).annotate(points=Count('workout__points'))
          #Filter for teams in list.
         else:
-            players = Player.objects.values('user__username', 'team__name').filter(team__name__in=teams)
-            workouts = Workout.objects.filter(Q(workoutpoint__time__gt=time) & Q(player__team__name__in=teams)).distinct()
-            # return Player.objects.values('user__username', 'team__name').filter(workout__workoutpoint__time__gte=time, team__name__in=teams).annotate(points=Count('workout__points', default=0)).order_by('-points')
+            players = Player.objects.values('user__username').filter(team__name__in=teams)
+            # workouts = Workout.objects.filter(Q(workoutpoint__time__gt=time) & Q(player__team__name__in=teams)).distinct()
+            players = Player.objects.values('user__username', 'team__name').filter(workout__workoutpoint__time__gte=time, team__name__in=teams).annotate(points=Count('workout__points')
         
+        all_players  = Player.objects.all()
+
+        ret_val = []
+        for p in all_players:
+            if p["user__username"] in players:
+                res = {"name": player["user__username"],
+                    "team": player["team__name"],
+                    "score": player["score"]}
+                ret_val.append(res)
+
         ret_val = []
         for player in players:
             res = {"name": player["user__username"],
