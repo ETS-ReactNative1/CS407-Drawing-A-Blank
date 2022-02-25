b1030e4ff1b285b14fb8391ac01f742cb148c232
Tal-Four <adamjonplatt@gmail.com>
2022-02-25 12:51:34 +0000

    formatting

--- back-end/mysite/apifile/leaderboards.py
+++ back-end/mysite/apifile/leaderboards.py
@@ -4,8 +4,6 @@ from django.db.models import Q
 
 
 def distance_leaderboard(time_range, teams):
-    workouts = None
-    players = None
 
     # Get all players/workouts from all teams
     if teams is None or teams == []:
@@ -20,22 +18,17 @@ def distance_leaderboard(time_range, teams):
     # initialize the dictionary/hashmap.
     dist_leaderboard = {}
     for player in players:
-        dist_leaderboard[player.user.username] = {"team" : player.team.name, "score": 0.0}
+        dist_leaderboard[player.user.username] = {"team": player.team.name, "score": 0.0}
 
-    #go through all workouts.
+    # go through all workouts.
     for workout in workouts:
         dist_leaderboard[workout.player.user.username]["score"] += stats.calc_workout_distance(workout)
 
     # sort distance dictionary
     sorted_dict = {k: v for k, v in sorted(dist_leaderboard.items(), key=lambda item: item[1]["score"], reverse=True)}
 
-    #update format to an array of dicts.
+    # update format to an array of dicts.
     return_array = []
     for key, value in sorted_dict.items():
-        dict_cur = {}
-        dict_cur["name"] = str(key)
-        dict_cur["team"] = value["team"]
-        dict_cur["score"] = value["score"]
-        return_array.append(dict_cur)
+        return_array.append({"name": str(key), "team": value["team"], "score": value["score"]})
     return return_array
-
