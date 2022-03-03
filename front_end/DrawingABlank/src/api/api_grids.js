e33cc1c75cc3ebdf587fc07e1c7d41358eea29d3
TBCNC <chasjohnh@gmail.com>
2022-03-03 14:17:23 +0000

    Added back-end integration to loading screen

--- front_end/DrawingABlank/src/api/api_grids.js
+++ front_end/DrawingABlank/src/api/api_grids.js
@@ -1,13 +1,9 @@
 import { request, getToken } from "./api_networking.js";
 
 export const getGrids = (bottom_left, top_right) => {
-    body = {
-        "bottom_left":bottom_left,
-        "top_right":top_right,
-        "zoom":1
-    };
-    console.log("Sending grid window request with:"+JSON.stringify(body));
-    return getToken().then(token => request('POST','map/collect/','',JSON.stringify(body),token))
+    query_string = `?bottom_left=${bottom_left}&top_right=${top_right}&zoom=10`
+    console.log(query_string);
+    return getToken().then(token => request('GET','map',query_string,'',token))
     .then(response => {
         if(response.status != 200){
             throw new Error('Could not retrieve grids.');
--- front_end/DrawingABlank/src/components/account_ui/loading_screen/loading_screen.js
+++ front_end/DrawingABlank/src/components/account_ui/loading_screen/loading_screen.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import {Text, View, TextInput, Button, TouchableOpacity, Touchable, ActivityIndicator} from 'react-native';
 import {styles} from './style.js';
-
+import Geolocation from 'react-native-geolocation-service';
+import {getEvents} from '../../../api/api_events.js';
+import {getGrids} from '../../../api/api_grids.js';
+const DEBUG_ZOOM_LEVEL = 0.01;
 class LoadingScreen extends Component{
     state={
-        loading_text:"Retrieving map info"
+        loading_text:"Obtaining location",
+        events_result:[],
+        map_result:[],
     }
     componentDidMount(){
-        //Perform requests here
+        //Perform requests here. Requests to do: get map info and get events (maybe cache workout history and event history?)*
+
+        Geolocation.getCurrentPosition(({coords}) => {
+            this.setState({loading_text:"Obtaining map info"});
+            getGrids(`${coords.latitude - DEBUG_ZOOM_LEVEL},${coords.longitude - DEBUG_ZOOM_LEVEL}`
+            ,`${coords.latitude + DEBUG_ZOOM_LEVEL},${coords.longitude + DEBUG_ZOOM_LEVEL}`)
+            .then(result => {this.setState({map_result:result, loading_text:"Retrieving event info"});console.log("GRID AMOUNT:"+result.length)})
+            .then(_ => {
+                getEvents().then(res => {
+                    this.setState({events_result:res, loading_text:"Done"});
+                });
+            });
+        });
     }
     render(){
         return(
