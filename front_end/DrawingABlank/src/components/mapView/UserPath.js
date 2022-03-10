b235d14a186df7b45a71022acd29c3eb46bc9a41
Chris24 <45335524+Chris24q@users.noreply.github.com>
2022-03-10 19:11:58 +0000

    gitignore

--- front_end/DrawingABlank/src/components/mapView/UserPath.js
+++ front_end/DrawingABlank/src/components/mapView/UserPath.js
@@ -1,8 +1,25 @@
 import React, {useRef, useState} from 'react';
+import {Workout} from '../workout_recording/workout';
+import useGeoLocation from './useGeoLocation';
 
-function UserPath() {
+const recorder = new Workout();
+
+function UserPath(isTracking) {
+  const userLocation = useGeoLocation();
   const [userPath, setUserPath] = useState();
-  userPath = useRef();
+  userPathRef = useRef();
+
+  useEffect(() => {
+    const {latitude, longitude} = userLocation.current;
+    const oldUserPath = userPathRef.current;
+    recorder.addCoordinate(latitude, longitude);
+
+    //draw new user movement polygon - map their travelled path
+    userPathRef.current = [...oldUserPath, {latitude, longitude}];
+    setUserPath(userPathRef.current);
+
+    return () => {};
+  }, [userLocation.current]); // not a state - relies on other state changes to see locaiton changes
 
   DrawUserPath = () => {};
 
