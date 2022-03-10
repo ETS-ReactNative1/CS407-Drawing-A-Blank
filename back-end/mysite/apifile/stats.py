a1963bbf847ed502bbe73ff53713c6d15348bb52
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-03-10 11:05:22 +0000

    testing point return

--- back-end/mysite/apifile/stats.py
+++ back-end/mysite/apifile/stats.py
@@ -55,11 +55,8 @@ def user_total_distance(input_name):
 
 def user_total_points(input_name):
     time = datetime.min
-
-    workouts = Workout.objects.values("player__user__username").filter(player__user__username=input_name, workoutpoint__time__gte=time).annotate(
-        score=Sum('points'))
     
-    # workouts = Player.objects.values('user__username').filter(user__username=input_name, workout__workoutpoint__time__gte=time).annotate(score=Sum('workout__points'))
+    workouts = Player.objects.values('user__username').filter(user__username=input_name).annotate(score=Sum('workout__points'))
     
     if workouts.exists():
         return workouts[0]["score"]
