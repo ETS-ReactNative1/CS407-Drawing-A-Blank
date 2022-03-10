b5f2143396002981eb859903e53f7925ea93cfd0
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-03-10 10:33:31 +0000

    typo

--- back-end/mysite/apifile/stats.py
+++ back-end/mysite/apifile/stats.py
@@ -57,7 +57,7 @@ def user_total_points(input_name):
     workouts = Workout.objects.values("player__user__username").filter(player__user__username=input_name).annotate(
         score=Sum('points'))
     if workouts.exists():
-        return workouts[0]["points"]
+        return workouts[0]["score"]
     else:
         return 0
 
