0fdaa598bb879d4d560565c8d819027185402a80
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-03-10 10:40:43 +0000

    formatting

--- back-end/mysite/apifile/stats.py
+++ back-end/mysite/apifile/stats.py
@@ -53,9 +53,9 @@ def user_total_distance(input_name):
 
 
 def user_total_points(input_name):
-    
     workouts = Workout.objects.values("player__user__username").filter(player__user__username=input_name).annotate(
         score=Sum('points'))
+    
     if workouts.exists():
         return workouts[0]["score"]
     else:
