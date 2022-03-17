a43d6ad90ee445d1f23116eb1b23d85cdb920233
TBCNC <chasjohnh@gmail.com>
2022-03-17 18:49:58 +0000

    Finished integration of event history and added a date filter

--- front_end/DrawingABlank/src/components/events/EventHistory.js
+++ front_end/DrawingABlank/src/components/events/EventHistory.js
@@ -5,6 +5,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 import EventCard from './EventCard.js';
 import { getEventHistory } from '../../api/api_events.js';
 import { getTeam } from '../../api/api_networking.js';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 class EventHistory extends Component{
     state={
@@ -13,9 +14,23 @@ class EventHistory extends Component{
         defeats:0,
         points:0,
         collectedEvents:false,
-        userTeam:""
+        userTeam:"",
+        showDatePicker:false,
     }
     
+    setDate = (date) =>{
+        this.hideDatePicker();
+        this.setState({collectedEvents:false},()=>{this.getEvents(date.toLocaleString('en-GB').split(',')[0])});
+    }
+
+    showDatePicker = () =>{
+        this.setState({showDatePicker:true});
+    }
+
+    hideDatePicker = () =>{
+        this.setState({showDatePicker:false});
+    }
+
     determineTeamWinner = (event_scores) =>{
         if(event_scores.ocean > event_scores.windy && event_scores.ocean > event_scores.terra){
             return "ocean";
@@ -52,27 +67,41 @@ class EventHistory extends Component{
         return date_start_conv.toLocaleDateString() + " " + date_start_conv.toLocaleTimeString() + " - " + date_end_conv.toLocaleDateString() + " " + date_end_conv.toLocaleTimeString() ;
     }
 
-    componentDidMount(){
-        getEventHistory().then(result => {
+    getEvents = (date="") =>{
+        getEventHistory(date).then(result => {
             console.log("GOT EVENT HISTORY:"+JSON.stringify(result));
             this.setState({obtainedEvents:result},()=>{
-                getTeam().then(team => {
-                    this.setState({userTeam:team},()=>{
-                        var victoryTotal = this.getVictoryTotal(this.state.userTeam);
-                        var lossTotal = this.getLossesTotal(this.state.userTeam);
-                        var points = this.getPointsTotal();
-                        this.setState({victories:victoryTotal,defeats:lossTotal,points:points},()=>{
-                            this.setState({collectedEvents:true});
+                if(this.state.obtainedEvents.length!=0){
+                    getTeam().then(team => {
+                        this.setState({userTeam:team},()=>{
+                            var victoryTotal = this.getVictoryTotal(this.state.userTeam);
+                            var lossTotal = this.getLossesTotal(this.state.userTeam);
+                            var points = this.getPointsTotal();
+                            this.setState({victories:victoryTotal,defeats:lossTotal,points:points},()=>{
+                                this.setState({collectedEvents:true});
+                            })
                         })
                     })
-                })
+                }else{
+                    this.setState({collectedEvents:true});
+                }
             })
         });
+    }
+
+    componentDidMount(){
+        this.getEvents();
     }
 
     render(){
         return(
             <View style={styles.main}>
+                <DateTimePicker
+                    isVisible={this.state.showDatePicker}
+                    mode="date"
+                    onConfirm={this.setDate}
+                    onCancel={this.hideDatePicker}
+                />
                 {this.state.collectedEvents ? 
                 (<View>
                 <View style={styles.window_title}>
@@ -83,19 +112,16 @@ class EventHistory extends Component{
                     <Text style={styles.score_defeats}>{this.state.defeats} defeat(s)</Text>
                     <Text style={styles.score_points}>{this.state.points} points</Text>
                 </View>
+                <View style={styles.filter}>
+                    <TouchableOpacity onPress={this.showDatePicker}>
+                        <Text style={styles.filter_text}>Filter by end date</Text>
+                    </TouchableOpacity>
+                </View>
                 <ScrollView style={styles.event_board}>
-                    {/*}
-                    <EventCard points_terra={23} points_ocean={42} points_windy={32} points_user={3} team_user='ocean' date="21st February 2022 13:42 - 23rd February 2022 13:42"/>
-                    <EventCard points_terra={12} points_ocean={42} points_windy={5} points_user={6} team_user='ocean' date="21st February 2022 13:42 - 23rd February 2022 13:42"/>
-                    <EventCard points_terra={45} points_ocean={23} points_windy={455} points_user={7} team_user='ocean' date="21st February 2022 13:42 - 23rd February 2022 13:42"/>
-                    <EventCard points_terra={1} points_ocean={132} points_windy={1} points_user={1} team_user='ocean' date="21st February 2022 13:42 - 23rd February 2022 13:42"/>
-                    <EventCard points_terra={233} points_ocean={45} points_windy={1} points_user={12} team_user='ocean' date="21st February 2022 13:42 - 23rd February 2022 13:42"/>
-                    <EventCard points_terra={34} points_ocean={32} points_windy={2} points_user={5} team_user='ocean' date="21st February 2022 13:42 - 23rd February 2022 13:42"/>
-                    */}
-                    {this.state.obtainedEvents.map((info,index) => {
-                        return(<EventCard points_terra={info.performance.terra} points_ocean={info.performance.ocean} points_windy={info.performance.terra} points_user={info.performance.user} team_user={this.state.userTeam}
+                    {(this.state.obtainedEvents.length > 0) ? this.state.obtainedEvents.map((info,index) => {
+                        return(<EventCard key={info.id} points_terra={info.performance.terra} points_ocean={info.performance.ocean} points_windy={info.performance.terra} points_user={info.performance.user} team_user={this.state.userTeam}
                         date={this.getDateString(info.start,info.end)}/>);
-                    })}
+                    }) : <Text style={{fontFamily:'Ubuntu-Light',fontSize:24,color:'#fafafa'}}>We could not find any events within this time range. Go out there and take part in some!</Text>}
                 </ScrollView>
                 </View>) : <View><ActivityIndicator color="#fafafa" size="large"/></View>}
             </View>
