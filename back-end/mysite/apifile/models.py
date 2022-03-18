c361929540c4632b182473494f76d74aad9fc9dd
Jack Dawes <jack.dawes@warwick.ac.uk>
2022-03-18 13:11:30 +0000

    changing to static method

--- back-end/mysite/apifile/models.py
+++ back-end/mysite/apifile/models.py
@@ -377,7 +377,8 @@ class Workout(models.Model):
     points = models.PositiveIntegerField(default=0)  # number of grids touched in that workout
     date_recorded = models.DateTimeField(auto_now_add=True)
 
-    def user_workouts(self, username, date):
+    @staticmethod
+    def user_workouts(username, date):
         workouts = Workout.objects.filter(player__user__username=username, date_created__gte=date)
 
         return workouts
